9248f8fdbb052ac05961b6cfe934c0f7ccf2b7e4
Electrik-rich546456 <58092123+Electrik-rich546456@users.noreply.github.com>
2026-01-25 01:19:17 +0000

    Update MainFragmentStateAdapter.kt

--- android/app/src/main/java/com/diafyt/lazarus/ui/MainFragmentStateAdapter.kt
+++ android/app/src/main/java/com/diafyt/lazarus/ui/MainFragmentStateAdapter.kt
@@ -1,28 +1,23 @@
 package com.diafyt.lazarus.ui
 
-import android.util.Log
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import kotlin.reflect.KClass
 
-/**
- * Customized variant of FragmentStateAdapter instantiating fragments
- * according to the array of types passed.
- */
-class MainFragmentStateAdapter(
-    private val mainActivity: MainActivity,
-    /**
-     * classes of the fragments to be instantiated
-     */
-    private val fragments: Array<KClass<out AbstractMainFragment>>
-) : FragmentStateAdapter(mainActivity) {
+class MainFragmentStateAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
 
-    override fun getItemCount(): Int = fragments.size
+    // This checks matches your log error: "Unresolved reference: fragmentStore"
+    val fragmentStore = mutableMapOf<Int, Fragment>()
+
+    override fun getItemCount(): Int = 3
 
     override fun createFragment(position: Int): Fragment {
-        val ret = fragments[position].constructors.first().call()
-        mainActivity.fragmentStore[position] = ret
-        Log.d(javaClass.name, "Create fragment $ret for position $position")
-        return ret
+        val fragment = when (position) {
+            0 -> TemperatureFragment()
+            1 -> ProgrammingFragment()
+            else -> TutorialFragment()
+        }
+        fragmentStore[position] = fragment
+        return fragment
     }
 }
