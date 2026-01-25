844d1f07e1f6cd519b90d05318aec31b816ac50b
Electrik-rich546456 <58092123+Electrik-rich546456@users.noreply.github.com>
2026-01-25 01:22:07 +0000

    Update TutorialFragment.kt

--- android/app/src/main/java/com/diafyt/lazarus/ui/TutorialFragment.kt
+++ android/app/src/main/java/com/diafyt/lazarus/ui/TutorialFragment.kt
@@ -1,30 +1,19 @@
 package com.diafyt.lazarus.ui
 
-import android.nfc.Tag
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.diafyt.lazarus.R
-import com.diafyt.lazarus.utils.Util
 
-/**
- * Show usage information.
- */
-class TutorialFragment : AbstractMainFragment() {
-    override val tabTitleKey = R.string.tab_title_tutorial
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_tutorial, container, false)
+class TutorialFragment : Fragment(R.layout.fragment_tutorial) {
+    
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
-    override fun handleNfc(tag: Tag) {
-        Util.showInfoSnack(
-            view,
-            R.string.snackbar_tutorial_nfc_intent
-        )
+    // Fixes "Unresolved reference: showInfoSnack"
+    private fun showInfoSnack(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
-
 }
